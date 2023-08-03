4b77f65936d5bd6d4de183754e94150b293bc065
studio404pl <kontakt@studio404.pl>
2023-08-03 12:29:33 +0200

    PUB-7803 CLI command fix

--- s3-uploads.php
+++ s3-uploads.php
@@ -8,12 +8,10 @@ Version: 3.0.7.1-assmbly
 Author URI: https://hmn.md
 */
 
-// Ensure the AWS SDK can be loaded.
-if ( ! class_exists( '\\Aws\\S3\\S3Client' ) ) {
-    // Require AWS Autoloader file.
-    require_once dirname( __DIR__ ) . '/vendor/autoload.php';
-}
-
 require_once __DIR__ . '/inc/namespace.php';
 
-add_action( 'plugins_loaded', 'S3_Uploads\\init', 0 );
+add_action( 'plugins_loaded', 'S3_Uploads\\init' );
+
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+	WP_CLI::add_command( 's3-uploads', 'S3_Uploads\\WP_CLI_Command' );
+}
